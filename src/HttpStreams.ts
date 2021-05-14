ae954adab369a44b48f3415f0dd36c132c45853b
nishant <nishant.singh87@gmail.com>
2021-05-15 01:30:14 +0530

    Http stream

--- src/HttpStreams.ts
+++ src/HttpStreams.ts
@@ -1,7 +1,7 @@
 import http from 'http';
 
 const server = http.createServer((request, response) => {
-
+    request.pipe(response);
 });
 
 server.listen(3009);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 #!/usr/bin/env node
 
-import './FileStream';
+// import './FileStream';
+import './HttpStreams';
